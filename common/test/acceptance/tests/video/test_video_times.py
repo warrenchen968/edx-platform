e251b08aac860b950260fb0af811ce2510625823
Michael Youngstrom <myoungstrom@edx.org>
2018-04-02 16:24:48 -0400

    another attr import

--- common/test/acceptance/tests/video/test_video_times.py
+++ common/test/acceptance/tests/video/test_video_times.py
@@ -1,6 +1,8 @@
 """
 Acceptance tests for Video Times(Start, End and Finish) functionality.
 """
+from nose.plugins.attrib import attr
+
 from common.test.acceptance.tests.video.test_video_module import VideoBaseTest
 
 
